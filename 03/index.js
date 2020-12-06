3ce3bb0f6a053bd861cb56f697d9dbe2b8e8a8d4
Jacek Smolak <jacek.smolak@gmail.com>
2020-12-06 22:17:35 +0100

    Refactor D3 to apply T2 for it

--- 03/index.js
+++ 03/index.js
@@ -5,15 +5,13 @@ const toGrid = (rawData) => toLines(rawData).map((line) => line.split(''));
 const TREE = '#';
 const readPosition = ({ row, col }, map) => map[row][col];
 const isTree = (field) => field === TREE;
-const rightMoveSteps = 3;
-const downMoveSteps = 1;
 
-const move = ({ row, col }, stepsToGoRight, stepsToGoDown, numberOfColumns) => ({
-    row: row + stepsToGoDown,
-    col: (col + stepsToGoRight) % numberOfColumns
+const move = ({ row, col }, movementSpeed, numberOfColumns) => ({
+    row: row + movementSpeed.down,
+    col: (col + movementSpeed.right) % numberOfColumns
 });
 
-export const countEncounteredTrees = (rawData) => {
+export const countEncounteredTrees = (rawData, movementSpeed) => {
     const grid = toGrid(rawData);
     const numberOfColumns = grid[0].length;
     const numberOfRows = grid.length;
@@ -23,10 +21,10 @@ export const countEncounteredTrees = (rawData) => {
 
     for (let rowNumber = 0; rowNumber < numberOfRows; rowNumber++) {
         fieldsEncountered.push(readPosition(currentPosition, grid));
-        currentPosition = move(currentPosition, rightMoveSteps, downMoveSteps, numberOfColumns);
+        currentPosition = move(currentPosition, movementSpeed, numberOfColumns);
     }
 
     return fieldsEncountered.filter(isTree).length;
 };
 
-console.log('[D3T1] Encountered trees:', countEncounteredTrees(getFileContents(__dirname, './inputData.txt')));
+console.log('[D3T1] Encountered trees:', countEncounteredTrees(getFileContents(__dirname, './inputData.txt'), { right: 3, down: 1 }));
--- 03/index.test.js
+++ 03/index.test.js
@@ -15,7 +15,12 @@ describe('countEncounteredTrees', () => {
 #...##....#
 .#..#...#.#`;
 
-    it('should return a number of trees we encounter along the way', () => {
-        expect(countEncounteredTrees(inputData)).to.equal(7);
+    it('should return a number of trees we encounter along the way while moving 3 right, 1 down', () => {
+        const movementSpeed = {
+            right: 3,
+            down: 1
+        };
+
+        expect(countEncounteredTrees(inputData, movementSpeed)).to.equal(7);
     });
 });
