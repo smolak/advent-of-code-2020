e77597f18e05095fc8fa48b9e0cfc3e79d4c4e2b
Jacek Smolak <jacek.smolak@gmail.com>
2020-12-06 12:27:54 +0100

    Complete first day (both tasks)

---
+++ 01/index.js
@@ -0,0 +1,46 @@
+import inputData from './inputData';
+
+const searchedSum = 2020;
+let twoNumbersResult;
+let threeNumbersResult;
+
+const checkSum = (entries, expectedSum) => {
+    return expectedSum === entries.reduce((sum, entry) => sum + entry, 0);
+}
+const multiply = (entries) => entries.reduce((product, entry) => product * entry, 1);
+
+// If there was a need for custom number of sum of numbers that produce 2020, I would think about
+// some generic function. For those two only a little duplication is fine.
+
+inputData.some((entry, index) => {
+    return inputData.some((secondEntry, secondIndex) => {
+        if (index !== secondIndex) {
+            const numbers = [ entry, secondEntry ];
+
+            if (checkSum(numbers, searchedSum)) {
+                twoNumbersResult = multiply(numbers);
+
+                return true;
+            }
+        }
+    });
+});
+
+inputData.some((entry, index) => {
+    return inputData.some((secondEntry, secondIndex) => {
+        return inputData.some((thirdEntry, thirdIndex) => {
+            if (index !== secondIndex && index !== thirdIndex) {
+                const numbers = [ entry, secondEntry, thirdEntry ];
+
+                if (checkSum(numbers, searchedSum)) {
+                    threeNumbersResult = multiply(numbers);
+
+                    return true;
+                }
+            }
+        });
+    });
+});
+
+console.log('Two numbers result:', twoNumbersResult);
+console.log('Three numbers result:', threeNumbersResult);
