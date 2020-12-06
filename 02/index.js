5b84f3ef4a7559a2a0f2d22cf67cd404f5ab4147
Jacek Smolak <jacek.smolak@gmail.com>
2020-12-06 18:29:09 +0100

    Refactor before applying D2T2

--- 02/index.js
+++ 02/index.js
@@ -4,26 +4,30 @@ import path from 'path';
 const getFileContents = (filename) => fs.readFileSync(path.resolve(__dirname, filename), { encoding: 'utf8'});
 const toLines = (rawData) => rawData.trim().split('\n');
 
-const toModel = (entry) => {
-    const [rangeData, letterData, password] = entry.split(' ');
-    const [min, max] = rangeData.split('-');
-    const letter = letterData.charAt(0);
+const rentalPlacePasswordPolicy = {
+    toModel: (entry) => {
+        const [rangeData, letterData, password] = entry.split(' ');
+        const [min, max] = rangeData.split('-');
+        const letter = letterData.charAt(0);
 
-    return {
-        min,
-        max,
-        letter,
-        password
-    };
-}
+        return {
+            min,
+            max,
+            letter,
+            password
+        };
+    },
 
-const parseData = (rawData) => toLines(rawData).map(toModel);
-const isValidPassword = ({ min, max, letter, password }) => {
-    const letterOccurrences = password.split(letter).length - 1;
+    isValidPassword: (model) => {
+        const { min, max, letter, password } = model;
+        const letterOccurrences = password.split(letter).length - 1;
 
-    return letterOccurrences >= min && letterOccurrences <= max;
+        return letterOccurrences >= min && letterOccurrences <= max;
+    }
 };
 
-export const countValidPasswords = (rawData) => parseData(rawData).filter(isValidPassword).length;
+const countValidPasswords = (passwordPolicy) => (rawData) => toLines(rawData).map(passwordPolicy.toModel).filter(passwordPolicy.isValidPassword).length;
 
-console.log(countValidPasswords(getFileContents('inputData.txt')));
+export const countValidPasswordsAtSledRentalPlace = countValidPasswords(rentalPlacePasswordPolicy);
+
+console.log(countValidPasswordsAtSledRentalPlace(getFileContents('inputData.txt')));
--- 02/index.test.js
+++ 02/index.test.js
@@ -1,32 +1,32 @@
 import { expect } from 'chai';
-import { countValidPasswords } from './index';
+import { countValidPasswordsAtSledRentalPlace } from './index';
 
-const inputDataWithNoValidPasswords = `
+describe('countValidPasswords for old job at the sled rental place', () => {
+    const inputDataWithNoValidPasswords = `
 1-3 f: abcde
 1-3 b: cdefg
 2-9 a: ccccccccc
 `;
-const inputDataWithOneValidPasswords = `
+    const inputDataWithOneValidPasswords = `
 1-3 f: abcde
 1-3 b: cdefg
 2-9 c: ccccccccc
 `;
-const inputDataWithTwoValidPasswords = `
+    const inputDataWithTwoValidPasswords = `
 1-3 a: abcde
 1-3 b: cdefg
 2-9 c: ccccccccc
 `;
-const inputDataWithThreeValidPasswords = `
+    const inputDataWithThreeValidPasswords = `
 1-3 a: abcde
 1-3 c: cdefg
 2-9 c: ccccccccc
 `;
 
-describe('countValidPasswords', () => {
     it('should return the number of passwords that are valid', () => {
-        expect(countValidPasswords(inputDataWithNoValidPasswords)).to.equal(0);
-        expect(countValidPasswords(inputDataWithOneValidPasswords)).to.equal(1);
-        expect(countValidPasswords(inputDataWithTwoValidPasswords)).to.equal(2);
-        expect(countValidPasswords(inputDataWithThreeValidPasswords)).to.equal(3);
+        expect(countValidPasswordsAtSledRentalPlace(inputDataWithNoValidPasswords)).to.equal(0);
+        expect(countValidPasswordsAtSledRentalPlace(inputDataWithOneValidPasswords)).to.equal(1);
+        expect(countValidPasswordsAtSledRentalPlace(inputDataWithTwoValidPasswords)).to.equal(2);
+        expect(countValidPasswordsAtSledRentalPlace(inputDataWithThreeValidPasswords)).to.equal(3);
     });
 });
